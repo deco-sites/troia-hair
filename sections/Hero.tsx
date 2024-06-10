7e3f8b81f33b1a1848ec52e195e50fa84a287267
Karla Oshikawa <karla_hara@hotmail.com>
2024-06-11 07:34:20 +0900

    feat: hero bar

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -20,9 +20,9 @@ export default function HeroFlats({
   
 }: Props) {
   return (
-    <nav class="lg:container lg:mx-auto mx-4">
-      <div class="flex flex-col items-center gap-8">
-        <div class="flex w-full xl:container xl:mx-auto py-2 mx-5 md:mx-10 z-10 flex-col  justify-center text-center  gap-12 md:gap-20 items-center">
+    <nav class="lg:container lg:mx-auto ">
+      <div class="flex flex-col items-center">
+        <div class="flex w-full xl:container  pt-2  z-10 flex-col  justify-center text-center  items-center">
           {image && (
             <Image
               width={1300}
@@ -35,9 +35,9 @@ export default function HeroFlats({
             />
           )}
         </div>
-        <div>
+        <div class="flex justify-between bg-primary w-full h-14 px-10">
           {informationBar?.map((item) => (
-            <div>
+            <div class="flex justify-center items-center gap-5">
               <Image
                 width={41}
                 class=""
@@ -46,8 +46,10 @@ export default function HeroFlats({
                 decoding="async"
                 loading="lazy"
               />
-              <h5>{item.title}</h5>
-              <p>{item.subtitle}</p>
+              <div>
+                <h5 class="font-bold text-[10px] text-white">{item.title}</h5>
+                <p class="font-normal text-[10px] text-white">{item.subtitle}</p>
+              </div>
             </div>
           ))}
         </div>
