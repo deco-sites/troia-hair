f01f8c61702285f997ef9565281e0dc8eb06f862
Karla Oshikawa <karla_hara@hotmail.com>
2024-06-11 17:08:33 +0000

    Release staging environment with 0 changes made by

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -4,15 +4,17 @@
 
 import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
+import * as $$$$$$10 from "./sections/bannerBig.tsx";
+import * as $$$$$$11 from "./sections/bannersSmall.tsx";
 import * as $$$$$$0 from "./sections/BlogPosts.tsx";
 import * as $$$$$$1 from "./sections/Categories.tsx";
 import * as $$$$$$2 from "./sections/Faq.tsx";
 import * as $$$$$$3 from "./sections/Footer.tsx";
 import * as $$$$$$4 from "./sections/Header.tsx";
-import * as $$$$$$10 from "./sections/hero-1.tsx";
-import * as $$$$$$11 from "./sections/hero-2.tsx";
+import * as $$$$$$12 from "./sections/hero-1.tsx";
+import * as $$$$$$13 from "./sections/hero-2.tsx";
 import * as $$$$$$5 from "./sections/Hero.tsx";
-import * as $$$$$$12 from "./sections/hero2.tsx";
+import * as $$$$$$14 from "./sections/hero2.tsx";
 import * as $$$$$$6 from "./sections/ImageWithParagraph.tsx";
 import * as $$$$$$7 from "./sections/Logos.tsx";
 import * as $$$$$$8 from "./sections/Testimonials.tsx";
@@ -20,15 +22,17 @@ import * as $$$$$$9 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
+    "site/sections/bannerBig.tsx": $$$$$$10,
+    "site/sections/bannersSmall.tsx": $$$$$$11,
     "site/sections/BlogPosts.tsx": $$$$$$0,
     "site/sections/Categories.tsx": $$$$$$1,
     "site/sections/Faq.tsx": $$$$$$2,
     "site/sections/Footer.tsx": $$$$$$3,
     "site/sections/Header.tsx": $$$$$$4,
-    "site/sections/hero-1.tsx": $$$$$$10,
-    "site/sections/hero-2.tsx": $$$$$$11,
+    "site/sections/hero-1.tsx": $$$$$$12,
+    "site/sections/hero-2.tsx": $$$$$$13,
     "site/sections/Hero.tsx": $$$$$$5,
-    "site/sections/hero2.tsx": $$$$$$12,
+    "site/sections/hero2.tsx": $$$$$$14,
     "site/sections/ImageWithParagraph.tsx": $$$$$$6,
     "site/sections/Logos.tsx": $$$$$$7,
     "site/sections/Testimonials.tsx": $$$$$$8,
